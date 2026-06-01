b2707a397da72b33e39a99e0cc8df999a0bb6386
tulikakumari <tulikakashyap1999@gmail.com>
2026-06-01 17:44:18 +0530

    Fix TransactionUtils bean registration

--- src/main/java/com/example/digital_library/services/TransactionUtils.java
+++ src/main/java/com/example/digital_library/services/TransactionUtils.java
@@ -2,7 +2,9 @@ package com.example.digital_library.services;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TransactionUtils {
 
     @Value("${book.max-allowed}")
